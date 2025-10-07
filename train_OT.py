3bc2e017f84c8be0fa000dc3b0a6fb11e4aac019
qaqliu <102677615+qaqliu@users.noreply.github.com>
2025-10-07 18:44:35 -0400

    Create 'chkpt' directory if it doesn't exist

--- train_OT.py
+++ train_OT.py
@@ -4,6 +4,7 @@ from argparse import Namespace
 from src import get_data, model, training, utils
 import torch
 import wandb
+import os
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 parser = argparse.ArgumentParser(description='Load hyperparameters from a YAML file.')
@@ -14,6 +15,7 @@ with open(args_parsed.JKO_config, "r") as f:
     config = yaml.safe_load(f)
 
 if __name__ == '__main__':
+    os.makedirs('chkpt', exist_ok=True)
     N = config['data']['N']
     
     p_tensor = get_data.inf_train_gen(
